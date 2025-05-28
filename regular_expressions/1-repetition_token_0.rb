e5b3b11358dfefa7ab147715c29e17bf5e106530
root <e.ishimwe3@alustudent.com>
2025-05-27 23:30:29 -0700

    first commit

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -2,18 +2,15 @@
 
 # Check for argument
 if ARGV.empty?
-  puts "Usage: ruby 1-repetition_token_0.rb <input_string>"
   exit 1
 end
 
 # Get the input string
 input = ARGV[0]
 
-# Define the regex pattern (match strings like hbttn, hbtttn, etc.)
-pattern = /^hbtt+n$/
+# Define regex: "hb" + 2-5 't' + 'n'
+pattern = /^hb(t{2,5})n$/
 
-# Use .match? to test the pattern
-if input.match?(pattern)
-  puts input
-end
+# Print only if the input matches
+puts input if input.match?(pattern)
 
